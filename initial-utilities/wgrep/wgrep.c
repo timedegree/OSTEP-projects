fd6e45e3f19347a787f6da7795db34b6244e24cc
timedegree <timedegree12@gmail.com>
2024-11-16 22:52:18 +0800

    upload some code in wgrep.c

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -1,4 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {}
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    printf("wgrep: searchterm [file ...]\n");
+    exit(1);
+  } else {
+    argc--;
+    char *searching = *++argv;
+
+    while (--argc > 0) {
+      FILE *fp = fopen(*++argv, "r");
+      if (fp == NULL) {
+        printf("wgrep: cannot open file\n");
+        exit(1);
+      }
+
+      char *linebuffer = NULL;
+      int *len = 0;
+      while (getline(&linebuffer, &len, fp) == -1) {
+        exit(1);
+      }
+
+      fclose(fp);
+    }
+  }
+
+  return 0;
+}
