a2292ca352ada8709a0f0b5b5d72514b3c7b7d52
timedegree <timedegree12@gmail.com>
2024-11-17 15:56:56 +0800

    complete wgrep.c

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -2,28 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+void grep_line(FILE *fp, char *searching){
+  char *linebuffer = NULL;
+  size_t len = 0;
+
+  while(getline(&linebuffer, &len, fp) != -1){
+    char *substr = strstr(linebuffer, searching);
+    
+    if(substr != NULL){
+      printf("%s",linebuffer);
+    }
+
+    free(linebuffer);
+    linebuffer = NULL;
+  }
+
+  fclose(fp);
+}
+
 int main(int argc, char *argv[]) {
   if (argc == 1) {
     printf("wgrep: searchterm [file ...]\n");
     exit(1);
-  } else {
+  }
+
+  if (argc == 2){  
+    FILE *fp = stdin;
+    char *searching = *++argv;
+    
+    grep_line(fp, searching);
+  }
+
+  if (argc > 2){
     argc--;
     char *searching = *++argv;
-
+    
     while (--argc > 0) {
       FILE *fp = fopen(*++argv, "r");
       if (fp == NULL) {
         printf("wgrep: cannot open file\n");
         exit(1);
       }
-
-      char *linebuffer = NULL;
-      int *len = 0;
-      while (getline(&linebuffer, &len, fp) == -1) {
-        exit(1);
-      }
-
-      fclose(fp);
+      
+      grep_line(fp, searching);
     }
   }
 
