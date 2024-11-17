47c64223d67f485d9be07f4504396a65b3739b9f
timedegree <timedegree12@gmail.com>
2024-11-17 23:40:02 +0800

    upload imcomplete wzip.c

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<strings.h>
+#include<string.h>
 
 int main(int argc,char *argv[])
 {
@@ -8,10 +8,41 @@ int main(int argc,char *argv[])
     printf("wzip: file1 [file2 ...]\n");
     exit(1);
   }
-
+  
   if(argc > 1){
-    
-  }
+    int count = 1;
 
-  return 0;
+    while(--argc >0){
+      FILE *fp = fopen(*++argv,"r");
+      if(fp == NULL){
+        printf("wzip: cannot open file\n");
+        exit(1);
+      }
+
+      char prev = fgetc(fp);
+      if(prev == EOF){
+        fclose(fp);
+        continue;
+      }
+      char curr;
+      
+      while((curr = fgetc(fp)) != EOF){
+        if(curr != prev){
+          fwrite(&count,sizeof(int),1,stdout);
+          fwrite(&prev,sizeof(char),1,stdout);
+          count = 1;
+          prev = curr;
+        }
+        else{
+          count++;
+        }
+      }
+      
+      if(argc == 1){
+        fwrite(&count,sizeof(int),1,stdout);
+        fwrite(&prev,sizeof(char),1,stdout);
+      }
+      fclose(fp);
+    }
+  }
 }
